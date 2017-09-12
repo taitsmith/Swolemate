5803f2339c237ffd74ea7baa3ea813e2c2b5581e
tait <taitsmith@outlook.com>
2017-09-12 16:18:01 -0400

    initial commit

---
+++ app/src/main/java/com/taitsmith/swolemate/activities/MainActivity.java
@@ -0,0 +1,21 @@
+package com.taitsmith.swolemate.activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.taitsmith.swolemate.R;
+
+public class MainActivity extends AppCompatActivity {
+private AdView mAdView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mAdView = (AdView) findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        mAdView.loadAd(adRequest);
+    }
+}
